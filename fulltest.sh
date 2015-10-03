65c5ad2a7c86df62b7cd4878c4d1775402e7ee8e
Marco Pessotto <melmothx@gmail.com>
2015-10-03 15:41:45 +0200

    Cleaned up util script

--- fulltest.sh
+++ fulltest.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 set -e
-
-perl -I lib -I ../PDF-Imposition/lib -I ../Text-Amuse/lib -I ~/amw/EBook-EPUB-Lite/lib t/merged.t
-
-perl -I lib -I ../PDF-Imposition/lib -I ../Text-Amuse/lib -I ~/amw/EBook-EPUB-Lite/lib t/epub-toc.t
-
-perl -I lib -I ../PDF-Imposition/lib -I ../Text-Amuse/lib -I ~/amw/EBook-EPUB-Lite/lib t/compile-merged.t
-
-
 perl Makefile.PL
 TEST_WITH_LATEX=0 RELEASE_TESTING=1 make test
 TEST_WITH_LATEX=1 RELEASE_TESTING=1 make test

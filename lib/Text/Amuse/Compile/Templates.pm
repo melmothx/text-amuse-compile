ba9725f44c76fd27d6535fb5143b02a70e71f4d9
Marco Pessotto <melmothx@gmail.com>
2017-11-29 14:02:57 +0100

    Add CSS styles for secondary footnotes

--- lib/Text/Amuse/Compile/Templates.pm
+++ lib/Text/Amuse/Compile/Templates.pm
@@ -316,6 +316,10 @@ a {
    text-decoration: underline;
 }
 
+.table-of-contents a {
+   text-decoration: none;
+}
+
 [% IF html %]
 div#page {
    margin:20px;
@@ -486,23 +490,23 @@ a.footnote, a.footnotebody {
     font-size: 80%;
     line-height: 0;
     vertical-align: super;
+    text-decoration: none;
 }
 
-* + p.fnline {
-    margin-top: 3em;
+* + p.fnline, * + p.secondary-fnline {
     border-top: 1px solid black;
-    padding-top: 2em;
+    padding-top: 0.5em;
 }
 
-p.fnline + p.fnline {
-    margin-top: 1em;
+p.fnline + p.fnline,  p.secondary-fnline + p.secondary-fnline {
     border-top: none;
     padding-top: 0;
 }
 
-p.fnline {
+p.fnline, p.secondary-fnline {
     font-size: 80%;
 }
+
 /* end footnotes */
 
 EOF

ca49141949e78325f5b5697dd3c45d093e9476c7
Marco Pessotto <marco@pessotto.hr>
2022-01-15 12:56:24 +0100

    CJK support via xecjk package

--- lib/Text/Amuse/Compile/Fonts/Selected.pm
+++ lib/Text/Amuse/Compile/Fonts/Selected.pm
@@ -116,47 +116,160 @@ STANDARD
 \usepackage{bookmark}
 HYPERREF
     }
-    # main language
-    my $orig_lang = $args{lang} || 'english';
+    my %use_polyglossia = (
+                           afrikaans       => 1,
+                           albanian        => 1,
+                           amharic         => 1,
+                           arabic          => 1,
+                           armenian        => 1,
+                           asturian        => 1,
+                           basque          => 1,
+                           belarusian      => 1,
+                           bengali         => 1,
+                           bosnian         => 1,
+                           breton          => 1,
+                           bulgarian       => 1,
+                           catalan         => 1,
+                           coptic          => 1,
+                           croatian        => 1,
+                           czech           => 1,
+                           danish          => 1,
+                           divehi          => 1,
+                           dutch           => 1,
+                           english         => 1,
+                           esperanto       => 1,
+                           estonian        => 1,
+                           finnish         => 1,
+                           french          => 1,
+                           friulian        => 1,
+                           gaelic          => 1,
+                           galician        => 1,
+                           georgian        => 1,
+                           german          => 1,
+                           greek           => 1,
+                           hebrew          => 1,
+                           hindi           => 1,
+                           hungarian       => 1,
+                           magyar          => 1,
+                           icelandic       => 1,
+                           interlingua     => 1,
+                           italian         => 1,
+                           japanese        => 0,
+                           kannada         => 1,
+                           khmer           => 1,
+                           korean          => 0,
+                           kurdish         => 1,
+                           lao             => 1,
+                           latin           => 1,
+                           latvian         => 1,
+                           lithuanian      => 1,
+                           macedonian      => 1,
+                           malay           => 1,
+                           malayalam       => 1,
+                           marathi         => 1,
+                           mongolian       => 1,
+                           nko             => 1,
+                           norwegian       => 1,
+                           norsk           => 1,
+                           occitan         => 1,
+                           persian         => 1,
+                           farsi           => 1,
+                           piedmontese     => 1,
+                           polish          => 1,
+                           portuguese      => 1,
+                           portuges        => 1,
+                           romanian        => 1,
+                           romansh         => 1,
+                           russian         => 1,
+                           sami            => 1,
+                           sanskrit        => 1,
+                           serbian         => 1,
+                           slovak          => 1,
+                           slovenian       => 1,
+                           sorbian         => 1,
+                           spanish         => 1,
+                           swedish         => 1,
+                           syriac          => 1,
+                           tamil           => 1,
+                           telugu          => 1,
+                           thai            => 0,
+                           tibetan         => 1,
+                           turkish         => 1,
+                           turkmen         => 1,
+                           ukrainian       => 1,
+                           urdu            => 1,
+                           uyghur          => 1,
+                           vietnamese      => 1,
+                           welsh           => 1,
+                          );
+    my %cjk = (
+               japanese => 1,
+               korean => 1,
+               chinese => 1,
+              );
 
-    if ($orig_lang eq 'chinese') {
-        push @out, "\\usepackage[chinese, provide=*]{babel}";
-        push @out, "\\usepackage[heading=true]{ctex}";
-        # foreach my $slot (qw/main mono sans/) {
-        #     # original lang
-        #     push @out, "\\setCJK${slot}font{" . $self->$slot->name . "}";
-        # }
-        return join("\n", @out);
-    }
-
-    push @out, "\\usepackage{fontspec}";
-    push @out, "\\usepackage{polyglossia}";
-
+    # for our purposes it's the same.
     my %aliases = (
-                   # pre texlive-2020
-                   # macedonian => 'russian',
                    serbian => 'croatian',
                   );
 
-    my $lang = $aliases{$orig_lang} || $orig_lang;
-    my %langs = ($lang => 1, map { $aliases{$_} || $_  => 1 } @{ $args{others} || [] } );
-
-    push @out, "\\setmainlanguage{$lang}";
-    if (my @other_langs = sort grep { $_ ne $lang } keys %langs) {
-        push @out, sprintf('\\setotherlanguages{%s}', join(",", @other_langs));
+    my $lang = $args{lang} || 'english';
+    if (my $aliased = $aliases{$lang}) {
+        $lang = $aliased;
     }
 
-    foreach my $slot (qw/main mono sans/) {
-        # original lang
-        push @out, "\\set${slot}font" . $self->_fontspec_args($slot => $lang);
-    }
+    push @out, "\\usepackage{fontspec}";
 
-    foreach my $l (sort keys %langs) {
-        push @out, "\\newfontfamily\\${l}font" . $self->_fontspec_args(main => $l);
-    }
 
-    if ($args{bidi}) {
-        push @out, '\\usepackage{bidi}';
+#  right now we’re using Song for sans and Kai for sf
+# https://github.com/adobe-fonts/source-han-serif/releases/download/2.000R/SourceHanSerifCN.zip
+# https://github.com/adobe-fonts/source-han-sans/releases/download/2.004R/SourceHanSansCN.zip
+
+    if ($cjk{$lang}) {
+        # these will die with luatex. Too bad.
+        push @out, "\\usepackage[$lang, provide=*]{babel}";
+        push @out, "\\usepackage{xeCJK}";
+        foreach my $slot (qw/main mono sans/) {
+            # original lang
+            push @out, "\\setCJK${slot}font" . $self->_fontspec_args($slot => $lang);
+        }
+        my %fallback = (
+                        main => "DejaVu Serif",
+                        sans => "DejaVu Sans",
+                        mono => "DejaVu Sans Mono",
+                       );
+        foreach my $slot (sort keys %fallback) {
+            push @out, "\\set${slot}font{" . $fallback{$slot} . "}";
+        }
+    }
+    elsif (!$use_polyglossia{$lang}) {
+        # for these languages at the moment we can't combine langs.
+        push @out, "\\usepackage[$lang, provide=*]{babel}";
+        foreach my $slot (qw/main mono sans/) {
+            # original lang
+            push @out, "\\set${slot}font" . $self->_fontspec_args($slot => $lang);
+        }
+    }
+    else {
+        push @out, "\\usepackage{polyglossia}";
+        my %langs = ($lang => 1, map { $aliases{$_} || $_  => 1 } @{ $args{others} || [] } );
+        push @out, "\\setmainlanguage{$lang}";
+        if (my @other_langs = sort grep { $_ ne $lang } keys %langs) {
+            push @out, sprintf('\\setotherlanguages{%s}', join(",", @other_langs));
+        }
+
+        foreach my $slot (qw/main mono sans/) {
+            # original lang
+            push @out, "\\set${slot}font" . $self->_fontspec_args($slot => $lang);
+        }
+
+        foreach my $l (sort keys %langs) {
+            push @out, "\\newfontfamily\\${l}font" . $self->_fontspec_args(main => $l);
+        }
+
+        if ($args{bidi}) {
+            push @out, '\\usepackage{bidi}';
+        }
     }
     push @out, '';
     return join("\n", @out);
